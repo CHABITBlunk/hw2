0ffed882980cf3b43b647042013d1d60af097608
xfbm <cblunk097@gmail.com>
2023-10-03 10:15:21 -0700

    Consolidate testSpeeds again

--- Memory.java
+++ Memory.java
@@ -2,17 +2,22 @@ package cs250.hw2;
 
 public class Memory {
 
+  private static volatile long ramRunningTotal;
+
   public static void main(String[] args) {
     int size = Integer.valueOf(args[0]);
     int experiments = Integer.valueOf(args[1]);
     int seed = Integer.valueOf(args[2]);
-    testCacheSpeeds(size, experiments);
-    testRamSpeeds(size, experiments);
+
+    // Task 1
+    System.out.println("Task 1");
+    testSpeeds(size, experiments);
   }
 
-  public static void testCacheSpeeds(int size, int experiments) {
+  public static void testSpeeds(int size, int experiments) {
+    // cache
     long runningTotal = 0;
-    long totalTime = 0;
+    double totalTime = System.nanoTime();
     for (int j = 0; j < experiments; j++) {
       if (size % 2 == 0) {
         for (int i = 0; i < size; i++) {
@@ -24,22 +29,25 @@ public class Memory {
         }
       }
     }
-  }
+    totalTime = (System.nanoTime() - totalTime) / experiments;
+    System.out.printf("Regular: %d seconds\n", totalTime);
 
-  public static void testRamSpeeds(int size, int experiments) {
-    long runningTotal = 0;
-    long totalTime = 0;
+    ramRunningTotal = 0;
+    double ramTotalTime = System.nanoTime();
     for (int j = 0; j < experiments; j++) {
       if (size % 2 == 0) {
         for (int i = 0; i < size; i++) {
-          runningTotal -= size;
+          ramRunningTotal -= size;
         }
       } else {
         for (int i = 0; i < size; i++) {
-          runningTotal += size;
+          ramRunningTotal += size;
         }
       }
     }
+    ramTotalTime = (System.nanoTime() - ramTotalTime) / experiments;
+    System.out.printf("Volatile: %d seconds\n", ramTotalTime);
+    System.out.printf("Avg regular sum: %d\nAvg volatile sum: %d", runningTotal, ramRunningTotal);
   }
 
 }
